34e842a10d84119ec987747f5e6dc5b0bb04f61c
Fahim Muntashir <fahimasdf07@gmail.com>
2025-11-07 09:59:54 +0600

    added map.js

---
+++ map.js
@@ -0,0 +1,24 @@
+// const postsArray = [
+//   { id: 1, title: "Post One", author: "alex" },
+//   { id: 2, title: "Post Two", author: "sam" },
+//   { id: 3, title: "Post Three", author: "jordan" },
+//   { id: 4, title: "Post Four", author: "taylor" },
+//   { id: 5, title: "Post Five", author: "casey" },
+// ];
+
+// const lookupTable = postsArray.reduce((table, post) => {
+//   table[post.id] = post;
+//   return table;
+// }, {});
+
+// console.log(lookupTable);
+
+const events = [
+  { timestamp: "2025-10-22T10:01:00Z", event: "login" },
+  { timestamp: "2025-10-22T10:05:00Z", event: "click" },
+  { timestamp: "2025-10-22T10:10:00Z", event: "logout" },
+  { timestamp: "2025-10-22T10:15:00Z", event: "login" },
+  { timestamp: "2025-10-22T10:20:00Z", event: "click" },
+  { timestamp: "2025-10-22T10:25:00Z", event: "click" },
+  { timestamp: "2025-10-22T10:30:00Z", event: "login" },
+];
