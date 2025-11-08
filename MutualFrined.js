aeeba74de970e1573efa2305495c35f148061a7b
Fahim Muntashir <fahimasdf07@gmail.com>
2025-11-08 19:18:30 +0600

    Mutual friend problem

---
+++ MutualFrined.js
@@ -0,0 +1,17 @@
+// const USER_COUNT = 50000;
+
+// let userA = [];
+
+// let userB = [];
+
+// const createUser = (id) => ({ id: `user_${id}`, name: `User ${id}` });
+
+// for (let i = 0; i < USER_COUNT; i++) {
+//   userA.push(createUser(i));
+//   userB.push(createUser(i + 25000));
+// }
+
+// console.log("Before Mutual Friends Processing:");
+
+// console.log(userA);
+// console.log(userB);
--- cache.js
+++ cache.js
@@ -10,3 +10,45 @@
 
 // getUserData(1);
 // getUserData(1);
+
+// const cache = {};
+
+// function getUserData(id) {
+//   if (cache[id]) {
+//     console.log("Returning from cache...");
+//     return cache[id];
+//   }
+//   console.log("Fetching from database...");
+//   const result = { id, name: "fahim" };
+//   cache[id] = result;
+//   return result;
+// }
+
+// getUserData(1); // database থেকে আনবে
+// getUserData(1); // cache থেকে দেবে
+
+const dataCache = new Map();
+
+const ecxpensiveTask = (id) => {
+  console.log("ran the ecxpensive task for ", id);
+  return {
+    id: id,
+    data: `some data for id ": ${id}`,
+    timestamp: new Date().getTime(),
+  };
+};
+
+const getData = (id) => {
+  if (dataCache.has(id)) {
+    return;
+  }
+
+  const data = ecxpensiveTask(id);
+
+  dataCache.set(id, data);
+
+  return data;
+};
+
+console.log(getData(123));
+console.log(dataCache);
