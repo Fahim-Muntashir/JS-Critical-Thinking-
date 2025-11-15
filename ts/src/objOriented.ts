fb6d6bb2c1ddf936ce0c8f3bfbfa7e1b787cdf3f
Fahim Muntashir <fahimasdf07@gmail.com>
2025-11-15 10:57:56 +0600

    type gurar

--- ts/src/objOriented.ts
+++ ts/src/objOriented.ts
@@ -1,17 +1,107 @@
 //oop - class - object
 
-class Animal {
-  name: string;
-  species: string;
-  sound: string;
+// class Animal {
+//   name: string;
+//   species: string;
+//   sound: string;
 
-  constructor(name: string, species: string, sound: string) {
-    this.name = name;
-    this.species = species;
-    this.sound = sound;
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+//   makeSound() {
+//     console.log(`The animal is making sound`);
+//   }
+// }
+
+// const dog = new Animal("German Shepherd", "Dog", "Bark");
+
+// console.log(dog);
+
+// const cat = new Animal("Persian", "Cat", "Meow");
+// console.log(cat.makeSound());
+
+class Parent {
+  getSleep(numOfHours: number) {
+    {
+      numOfHours;
+    }
   }
 }
 
-const dog = new Animal("German Shepherd", "Dog", "Bark");
+// class Student {
+//   name: string;
+//   age: number;
+//   address: string;
+//   constructor(name: string, age: number, address: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//   }
 
-console.log(dog);
+//   getSleep(numOfHours: number) {
+//     console.log(`${this.name} is sleeping ${numOfHours}`);
+//   }
+// }
+
+// const student1 = new Student("Anik", 24, "Dhaka");
+// console.log(student1);
+// student1.getSleep(15);
+
+// class Teacher {
+//   name: string;
+//   age: number;
+//   address: string;
+//   designation: string; //extra freeking proprerty
+//   constructor(name: string, age: number, address: string, designation: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//     this.designation = designation;
+//   }
+
+//   getSleep(numOfHours: number) {
+//     console.log(`${this.name} is sleeping ${numOfHours}`);
+//   }
+
+//   takeClass(numOfClass: number) {
+//     console.log(`${this.name} eto ghonta class nen ${numOfClass}`);
+//   }
+// }
+
+// const teacher1 = new Teacher("Anik", 24, "Dhaka", "husband");
+// console.log(teacher1);
+// teacher1.takeClass(15);
+
+//type guard
+
+//in typeof
+
+const add = (num1: number | string, num2: number | string) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else {
+    num1.toString() + num2.toString;
+  }
+};
+
+add(2, 2);
+add(2, "2");
+
+//in guard
+
+// type NormalUser = {
+//   name: string;
+// };
+
+// type AdminUser = {
+//   name: string;
+//   role: "Admin";
+// };
+
+// const getUserInfo = (user: NormalUser | AdminUser) => {
+//   console.log(user);
+// };
+
+// getUserInfo({ name: "Normal" });
