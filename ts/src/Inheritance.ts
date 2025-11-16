12fd04d72734bd648303a9e4b954fa4ecfe83383
Fahim Muntashir <fahimasdf07@gmail.com>
2025-11-16 10:40:08 +0600

    inheritecne

---
+++ ts/src/Inheritance.ts
@@ -0,0 +1,26 @@
+class Vehicle {
+  brand: string;
+  constructor(brand: string) {
+    this.brand = brand;
+  }
+  start() {
+    console.log("Statring");
+  }
+}
+
+class Car extends Vehicle {
+  model: string;
+  constructor(brand: string, model: string) {
+    super(brand);
+    this.model = model;
+  }
+
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+//object
+const mycar = new Car("Toyota", "Corola");
+
+mycar.start();
