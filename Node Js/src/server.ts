2c9db425db4bbc7593457b858c5b68c256219f08
Fahim Muntashir <fahimasdf07@gmail.com>
2025-11-30 08:08:02 +0600

    added

---
+++ Node Js/src/server.ts
@@ -0,0 +1,21 @@
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
+
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log("server is running ....");
+    if (req.url == "/" && req.method == "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Hello from node js module",
+          path: req.url,
+        })
+      );
+    }
+  }
+);
+
+server.listen(config.port, () => {
+  console.log("server is listening on port");
+});
